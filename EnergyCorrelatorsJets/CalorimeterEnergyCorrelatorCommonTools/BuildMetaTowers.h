95ab5590f1a8fc2c2e234023a95827046d4c1be6
Silas K Grossberndt <silas.grossberndt@gmail.com>
2026-03-09 16:25:37 -0400

    Added first bit the of the build MetaTowers. Need a .cc file for it

---
+++ EnergyCorrelatorsJets/CalorimeterEnergyCorrelatorCommonTools/BuildMetaTowers.h
@@ -0,0 +1,28 @@
+#ifndef __BUILDMETATOWERS_H__
+#define __BUILDMETATOWERS_H__
+
+//////////////////////////////////////////////////////////////////////////
+//									//
+//			Build Meta Towers				//
+//									//
+// 	Does what is says on the tin, take in calorimeters, applies a   //
+// 	vertex based correction and combines the towers to make a	//
+// 	single "shifted em+hadronic calorimeter" w/ HCAL bins		//
+// 	Works with vandy skimmer data and Tower info			//
+//									//
+//	Authors: 	Ben Kimelman, Skaydi 				//
+//	First commit: 	9 March 2026					//
+//	This commit: 	9 March 2026					//
+//	version: 	v1.0						//
+//									//
+//	Notes on this commit: First commit				//
+//									//
+//////////////////////////////////////////////////////////////////////////
+
+
+//c++ classes
+
+#include <math.h>
+#include <vector>
+
+
